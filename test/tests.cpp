ec05aeff50ce33cfc2792caef4c89d3b68a1c696
David Antliff <david@pitchblende.co.nz>
2021-12-02 02:52:37 +1300

    Set up for C++20 & GTest.

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,14 +1,7 @@
-#include <catch2/catch.hpp>
+#include <gtest/gtest.h>
 
-unsigned int Factorial(unsigned int number)// NOLINT(misc-no-recursion)
-{
-  return number <= 1 ? number : Factorial(number - 1) * number;
-}
+class Tests : public ::testing::Test {};
 
-TEST_CASE("Factorials are computed", "[factorial]")
-{
-  REQUIRE(Factorial(1) == 1);
-  REQUIRE(Factorial(2) == 2);
-  REQUIRE(Factorial(3) == 6);
-  REQUIRE(Factorial(10) == 3628800);
+TEST_F(Tests, test_something) {
+    EXPECT_TRUE(0);
 }
